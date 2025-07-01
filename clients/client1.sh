4ca20e2586dc34d921910b0504654185d1b9b7d5
Darren Bono <dbono215@gmail.com>
2025-07-01 16:57:33 +0000

    feat: implement VRF RED for VLAN service isolation across routers and clients

--- clients/client1.sh
+++ clients/client1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "8021q" >> /etc/modules
 
+# create VLAN interface for RED service
 ip link add name eth1.10 link eth1 type vlan id 10
 ip link set dev eth1.10 up
 ip addr add 10.10.1.2/30 dev eth1.10
--- r2/r2.sh
+++ r2/r2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "8021q" >> /etc/modules
 
+# create VLAN interface for RED service
 ip link add name eth3.10 link eth3 type vlan id 10
 ip link set dev eth3.10 up
+
+# create RED VRF and attach the VLAN interface to it
+ip link add dev RED type vrf table 10
+ip link set dev eth3.10 master RED
+ip link set dev RED up
