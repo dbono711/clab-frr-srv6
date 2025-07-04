f79d93e290f8de2a071caef3fa8cc41470cd5917
Darren Bono <dbono215@gmail.com>
2025-07-04 21:07:27 +0000

    feat: enable SRv6 with ISIS segment routing and upgrade FRR to 10.3.1

--- r1/r1.sh
+++ r1/r1.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo "8021q" >> /etc/modules
 
+# enable SRv6
+sysctl net.ipv6.seg6_flowlabel=1
+sysctl net.ipv6.conf.all.seg6_enabled=1
+
+# enable VRF strict mode
+sysctl net.vrf.strict_mode=1
+
+# create SRv6 interface
+ip link add sr0 type dummy
+ip link set sr0 up
+
 # create VLAN interface for RED service
 ip link add name eth3.10 link eth3 type vlan id 10
 ip link set dev eth3.10 up
