ed22d64164d624945d5ead4c289e65c867c82d9c
Darren Bono <dbono215@gmail.com>
2025-07-14 09:02:53 -0400

    feat: add logging stack with Loki, Promtail and Grafana for router monitoring

--- r2/r2.sh
+++ r2/r2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# create logs directory
+mkdir -p /etc/frr/logs
+chown -R frr:frr /etc/frr/logs
+chmod 775 /etc/frr/logs
+
 # create SRv6 interface
 ip link add sr0 type dummy
 ip link set sr0 up
